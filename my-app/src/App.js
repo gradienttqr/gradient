7c4c84512bd8e5f406d0bfa64f0129e951f387c3
gradienttqr <161477043+gradienttqr@users.noreply.github.com>
2024-05-08 12:55:05 +0000

    color button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,6 @@ import { dataForList, dataForButtons } from './content/mainData.js';
 import Button from  './components/button/button.js';
 import './components/header/main/main.css';
 import { useState } from 'react';
-// import Main from './components/header/main/main.css'
 function App() {
   const [contentFromButton, setNewContent] = useState('About us');
 
@@ -22,12 +21,15 @@ function App() {
       <section>
         <dl>
         <div  class='text-white'>
-          {dataForList.map((item) => <MainList {...item} />)}
+          {dataForList.map((item) => <MainList {...item} />)} //Как ?
           </div>
         </dl>
       </section>
      <section>
      <body> 
+     <img>
+        src="https://w.forfun.com/fetch/54/544be23a1118c704f56e34448b51488d.jpeg" alt=""  //Почему ?
+      </img>
     </body>
         <h1> All buttons </h1>
         <Button onClick={() => handleClick('prop1')}>Item 1</Button>
